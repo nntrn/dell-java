913c6cddfd542c2c8b4e0212d29fbe2b090a2ca2
annie tran <nntrn@me.com>
2019-01-07 02:56:17 -0600

    calculator

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,48 +1,51 @@
-//Ask the user for 2 numbers.
-//Print out the sum of the 2 numbers.
-//Print out the difference of the 2 numbers.
-//Print out the product of the 2 numbers.
-//Print out the quotient of the 2 number.
-
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in); 
-        
-        double sum = 0;
-        double difference = 0;
-        double product = 0;
-        double quotient = 0;
-        
-  
-        System.out.println("provide calculator w/ two numbers, separate using a space ('exit' to close)");
-        while (!sc.hasNext("exit")) {
-            Scanner lineSc = new Scanner(sc.nextLine());
-            double num1 = 0;
-            double num2 = 0;
- 
-            while (lineSc.hasNextDouble()) {
-                num1 += lineSc.nextDouble();
-                num2 += lineSc.nextDouble();
-            }
-            
-            sum = num1 + num2; 
-            difference = num1 - num2; 
-            product = num1 * num2;
-            quotient = num1 / num2;
-            
-            System.out.println("sum is " + sum);
-            System.out.println("difference is " + difference);
-            System.out.println("product is " + product);
-            System.out.println("quotient is " + quotient);
-            lineSc.close();
-            
-        }
-        
-		sc.close();
+		double num1 = getNumberInputs();
+		double num2 = getNumberInputs();
+
+		calcSum(num1, num2);
+		calcDifference(num1, num2);
+		calcProduct(num1, num2);
+		calcQuotient(num1, num2);
+
+	}
+
+	public static double getNumberInputs() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter value:");
+		
+		// advance the Scanner until it hasNextDouble()
+		// removing sc.next() would cause an infinite loop
+		while (!sc.hasNextDouble()) {
+			System.out.println("only double data types allowed");
+			sc.next();
+		}
+		return sc.nextDouble();
+
+	}
+
+	public static double calcSum(double num1, double num2) {
+		System.out.println("addition: " + (num1 + num2));
+		return num1 + num2;
+	}
+
+	public static double calcDifference(double num1, double num2) {
+		System.out.println("subtraction: " + (num1 - num2));
+		return num1 - num2;
+	}
+
+	public static double calcProduct(double num1, double num2) {
+		System.out.println("multiplication " + (num1 * num2));
+		return num1 * num2;
+	}
+
+	public static double calcQuotient(double num1, double num2) {
+		System.out.println("division: " + (num1 / num2));
+		return num1 / num2;
 	}
 
 }
