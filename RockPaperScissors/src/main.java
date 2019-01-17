ed9d58757d62ff321bcd7e4884ddaaf593e627ad
annie tran <nntrn@me.com>
2019-01-16 18:54:42 -0600

    update draft

--- RockPaperScissors/src/main.java
+++ RockPaperScissors/src/main.java
@@ -1,4 +1,48 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class main {
+	
+	public static void main(String[] args) {
 
+		String user = getMoveForUser();
+		String comp = getMoveForComp();
+		System.out.println(user);
+
+
+	}
+	
+	public static String getMoveForUser() {
+		String[] handMoves = {"rock", "paper", "scissors"};
+
+		Scanner sc = new Scanner(System.in);
+		
+		String userMove = sc.next().toLowerCase();
+		
+		// input check to prompt user for valid hand moves
+		while(!Arrays.stream(handMoves).anyMatch(userMove::equals))  {
+			System.out.println("need to be: rock, paper, or scissors");
+			userMove = sc.next().toLowerCase();
+		}
+		return userMove;
+		
+	}
+	
+	public static String getMoveForComp() {
+		String[] handMoves = {"rock", "paper", "scissors"};
+
+		Scanner sc = new Scanner(System.in);
+		
+		String userMove = sc.next().toLowerCase();
+		
+		// input check to prompt user for valid hand moves
+		while(!Arrays.stream(handMoves).anyMatch(userMove::equals))  {
+			System.out.println("need to be: rock, paper, or scissors");
+			userMove = sc.next().toLowerCase();
+		}
+		return userMove;
+		
+	}
+	
 }
