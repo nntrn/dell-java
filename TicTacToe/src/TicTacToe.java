a97ba4217ee179e9327fc9006fcdecbcaa9a5572
annie tran <nntrn@me.com>
2019-01-24 03:31:08 -0600

    add Javadoc comments

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class TicTacToe {
+/**
+ * The TicTacToe class implements an application for a two player tic tac toe
+ * game. User specify position using values 1 through 9
+ */
+class TicTacToe {
 
 	// GLOBAL VARIABLES
 	public static String[] board = new String[9];
-	public static String num[] = "1,2,3,4,5,6,7,8,9".split(",");
-
+	public static String validBoardPositions[] = "1,2,3,4,5,6,7,8,9".split(",");
 	public static int move = 1;
 
 	public static void main(String[] args) {
 
 		System.out.println("TIC TAC TOE");
-		System.out.println("instructions: specify where on the board you want to move using 1-9");
+		System.out.println("instructions: specify position on board 1-9\n");
 
 		Arrays.fill(board, "   ");
 
@@ -32,12 +35,16 @@ public class TicTacToe {
 
 			gameResult(detectWin, user);
 		}
-
-		System.out.println("\n----END----");
+		System.out.println("----END----");
 
 	}
 
-	static void getMoveForUser(String playerTurn) {
+	/**
+	 * Accepts position inputs that exist in validBoardPositions[]
+	 * 
+	 * @param playerTurn (X or O)
+	 */
+	public static void getMoveForUser(String playerTurn) {
 
 		move++;
 
@@ -45,20 +52,28 @@ public class TicTacToe {
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
 
-		while (!Arrays.stream(num).anyMatch(input::equals)) {
+		while (!Arrays.stream(validBoardPositions).anyMatch(input::equals)) {
 			System.out.println("***invalid position, try again***");
 			input = sc.nextLine();
 		}
 
 		int i = Integer.parseInt(input) - 1;
-		num[i] = playerTurn;
+
+		validBoardPositions[i] = playerTurn; // remove position so it cant be used again
 		board[i] = playerTurn;
 
 	}
 
+	/**
+	 * @param player (X or O)
+	 * @return boolean if win is detected
+	 */
 	public static boolean checkForWin(String player) {
 
-		String[] winMoves = { "0;1;2", "3;4;5", "6;7;8", "0;3;6", "1;4;7", "2;5;8", "0;4;8", "2;4;6" };
+		String[] winMoves = { "0;1;2", "3;4;5", "6;7;8", // horizontal wins
+													"0;3;6", "1;4;7", "2;5;8", // vertical wins
+													"0;4;8", "2;4;6" 					 // diagonal win
+												};
 
 		for (int i = 0; i < winMoves.length; i++) {
 
@@ -72,12 +87,16 @@ public class TicTacToe {
 
 	}
 
+	/**
+	 * @param win    boolean condition from checkForWin
+	 * @param player (X or O)
+	 */
 	static void gameResult(boolean win, String player) {
 
 		if (win) {
 			System.out.println("\n-----------------");
 			System.out.println("PLAYER" + player + "WINS!");
-			System.out.println("-----------------");
+			System.out.println("-----------------\n");
 		}
 
 		if (move == 9)
