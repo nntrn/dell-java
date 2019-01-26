da99158aa6082c0f170579d036f5bd705c564a5b
Annie Tran <annie@annies-mbp-2.attlocal.net>
2019-01-26 00:10:34 -0600

    use Builder pattern for DriverLicense

--- DriverLicense/src/DriverLicense.java
+++ DriverLicense/src/DriverLicense.java
@@ -1,33 +1,108 @@
 import java.util.Arrays;
 import java.time.*;
 
+/**
+ * this DriverLicense class initial
+ *
+ */
 public class DriverLicense {
 
-	private String _firstName;
-	private String _lastName;
-	private String _dob; // make user follow format MM/DD/YYYY
-	private int _height; // make user use inches
-	private char _gender;
-	
-	
-	public String getFullName(String fname, String lname) {
+	private String fname;
+	private String lname;
+	private String dob;
+	private String height;
+	private String gender;
+
+	/**
+	 * returns null if first or last names are missing
+	 * 
+	 */
+	public String getFullName() {
+
+		if (fname == null || lname == null)
+			return null;
+
 		return fname + " " + lname;
 	}
-	
-	
-	public int getAge(String date) {
-		// TODO add error handling
-		LocalDate today = LocalDate.now(); 
-		
-		int[] dob = Arrays.stream(date.split("/")).mapToInt(Integer::parseInt).toArray();
-		
-		LocalDate birthday = LocalDate.of(dob[2], dob[0], dob[1]); 
-		
-		Period period = Period.between(birthday, today);
-		int age = period.getYears();
-		
-		return age;
+
+	/**
+	 * returns age if correct date format is used: MM/DD/YYYY otherwise -1 is
+	 * returned 
+	 * TODO: return correct years when MM/DD/YY is used
+	 * 
+	 */
+	public int getAge() {
+		try {
+			int[] arr = Arrays.stream(dob.split("/")).mapToInt(Integer::parseInt).toArray();
+			LocalDate birthday = LocalDate.of(arr[2], arr[0], arr[1]);
+
+			return Period.between(birthday, LocalDate.now()).getYears();
+
+		} catch (Exception e) {
+			return -1;
+		}
+
 	}
-	
-	
+
+	/**
+	 * Builder class
+	 * 
+	 */
+	public static class Builder {
+
+		private String fname;
+		private String lname;
+		private String dob;
+		private String height;
+		private String gender;
+
+		public Builder() {
+		}
+
+		public Builder setFirstName(String fname) {
+			this.fname = fname;
+			return this;
+		}
+
+		public Builder setLastName(String lname) {
+			this.lname = lname;
+			return this;
+		}
+
+		public Builder setDOB(String dob) {
+			this.dob = dob;
+			return this;
+		}
+
+		public Builder setHeight(String height) {
+			this.height = height;
+			return this;
+		}
+
+		public Builder setGender(String gender) {
+			this.gender = gender;
+			return this;
+		}
+
+		public DriverLicense build() {
+
+			DriverLicense dl = new DriverLicense();
+
+			dl.fname = this.fname;
+			dl.lname = this.lname;
+			dl.dob = this.dob;
+			dl.height = this.height;
+			dl.gender = this.gender;
+			return dl;
+		}
+	}
+
+	/**
+	 * constructor is made private so that the only way to initialize it is via the
+	 * Builder class.
+	 */
+	public DriverLicense() {
+
+	}
+
 }
--- DriverLicense/src/LicenseProgram.java
+++ DriverLicense/src/LicenseProgram.java
@@ -1,12 +1,38 @@
 
+
+/**
+ * the LicenseProgram class runs and creates DriversLicense instances.
+ * The LicenseProgram class will contain one main method (and nothing else).
+ * When run, LicenseProgram should create three different instances of a DriversLicense 
+ * and print out the full name and age on each license. 
+ * 
+ * */
 public class LicenseProgram {
 	public static void main(String[] args) {
-		
-		DriverLicense dr = new DriverLicense();
-		
-		int age = dr.getAge("02/01/1992");
-		
-		System.out.println(age);
 			
+		DriverLicense DL1 = new DriverLicense.Builder()
+				.setFirstName("annie")
+				.setLastName("tran")
+				.setDOB("02/08/1994")
+				.setHeight("5'4")
+				.setGender("F")
+				.build();
+		
+		// object to demonstrate missing last name
+		DriverLicense DL2 = new DriverLicense.Builder()
+				.setFirstName("annie tran")
+				.setDOB("02/08/1994")
+				.build();
+		
+		// object to demonstrate incorrect string format for DOB
+		DriverLicense DL3 = new DriverLicense.Builder()
+				.setFirstName("annie")
+				.setDOB("tran")
+				.build();
+		
+		System.out.println(DL1.getFullName() +": " + DL1.getAge());
+		System.out.println(DL2.getFullName() +": " + DL2.getAge());
+		System.out.println(DL3.getFullName() +": " + DL3.getAge());
+		
 	}
 }
