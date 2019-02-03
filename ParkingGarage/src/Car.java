4dd1c8df28eb34298e928cf86f88d1e0e2a0362e
annie tran <nntrn@me.com>
2019-02-02 22:04:03 -0600

    car class

--- ParkingGarage/src/Car.java
+++ ParkingGarage/src/Car.java
@@ -1,36 +1,65 @@
+
 /**
- * Car has the following attributes:
- * Color, License#, Make, and Model
- * 
- *  https://howtodoinjava.com/design-patterns/behavioral/mediator-pattern/
- *  
+ * represents a car
  */
 public class Car {
-	
-	public Car color;
-	//public String licenseNum;
-	//public String make;
-	public Car model;
-	
-    // the Bicycle class has one constructor
-    public Car() {
-    	
-    	//licenseNum = startCadence;
-    	//make = startSpeed;
-    	model = getModel();
-    	
-    	Car d = new Car();
-    	d.color = getColor();
-    	d.model = getModel();
-    }
-    
-	public Car getColor() {
-		return this;
-	}
-	
-	public Car getModel() {
-		return this;
+
+	String license;
+	String color;
+	String make;
+	String model;
+
+	/**
+	 * this is the constructor of the class Car
+	 */
+	public Car(String license) {
+		this.license = license.toUpperCase();
 	}
 
+	/**
+	 * assigns color to car
+	 */
+	public void color(String carColor) {
+		color = carColor;
+	}
 
+	/**
+	 * assign car make
+	 */
+	public void make(String carMake) {
+		make = carMake;
+	}
+
+	/**
+	 * assign car model
+	 */
+	public void model(String carModel) {
+		model = carModel;
+	}
+
+	/**
+	 * Print the Car details
+	 */
+	public void printCar() {
+		System.out.print(license + ", ");
+		System.out.print(color + ", ");
+		System.out.print(make + ", ");
+		System.out.print(model + ", ");
+	}
 }
+
+/**
+ * represent available parking spots
+ */
+class ParkingGarage{
+	
+	int capacity = 10;
+	
+	public ParkingGarage() {
+		this.capacity = this.capacity -1;
+		System.out.print(capacity);
+	}
+
+	
+}
+
--- ParkingGarage/src/GarageManager.java
+++ ParkingGarage/src/GarageManager.java
@@ -1,4 +1,19 @@
 
 public class GarageManager {
+	
+	public static void main(String args[]) {
+	      /* Create two objects using constructor */
+	      Car c1 = new Car("3jsofs");
+	      Car c2 = new Car("ajskaji");
+
+	      // Invoking methods for each object created
+	      c1.color("black");
+	      c1.model("civic");
+	      c1.make("honda");
+	      
+	      c1.printCar();
+	      
+	      ParkingGarage p = new ParkingGarage();
+	   }
 
 }
