8520075abe268bf9b2c2b4501597bad69e8fe8a9
annie tran <nntrn@me.com>
2019-02-13 18:29:24 -0600

    readme

--- Rentable/src/RentableProgram.java
+++ Rentable/src/RentableProgram.java
@@ -1,4 +1,36 @@
+import java.io.*;
+import java.util.*;
+
 
 public class RentableProgram {
+	
+
+    
+    public static void main(String[] args) {
+    	
+
+        Stack<String> STACK = new Stack<String>(); 
+        Scanner scanner = new Scanner(System.in);
+    	
+    	STACK.push("ajasl");
+    	STACK.push("asd");
+    	STACK.push("asd");
+    	
+    	System.out.println(STACK);
+
+    }
+
+    public Stack<String> getCodeString(){
+      
+      String s = scanner.next();
+      STACK.push(s);
+
+      System.out.println(s);
+
+      return STACK;
+
+    }
+    
+    
 
 }
